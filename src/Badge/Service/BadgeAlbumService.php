a82306bba1e65c3517c4fd016e5662e112c12e96
Raizer <bryanvdstarre@gmail.com>
2022-03-24 01:43:39 +0100

    test commit

--- src/Badge/Service/BadgeAlbumService.php
+++ src/Badge/Service/BadgeAlbumService.php
@@ -31,7 +31,7 @@ class BadgeAlbumService
      */
     public function execute(): bool|array
     {
-        if(!is_dir( $this->badgeHelper->getPath())) {
+        if(!is_dir($this->badgeHelper->getPath())) {
             return false;
         }
 
