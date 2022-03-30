2ecb9db68be9896ee6034ee3782d4b5d852b2014
Domexx <dominik-forschner@web.de>
2022-03-30 17:24:09 +0200

    [TASK] Updated packages and project to php 8.1

--- public/index.php
+++ public/index.php
@@ -5,4 +5,7 @@
  * @see LICENSE (MIT)
  */
 
+/** @TODO change error reporting when package updates are done */
+error_reporting(E_ALL ^ E_DEPRECATED);
+
 (require __DIR__ . '/../app/bootstrap.php')->run();
